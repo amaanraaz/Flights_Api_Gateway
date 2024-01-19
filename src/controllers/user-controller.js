9ab69f71e626bafa64e8a0b96d87342ff4799ca7
amaanraaj <amandwivedi00@gmail.com>
2024-01-19 20:02:00 +0530

    signup user api created

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,24 @@
+const { StatusCodes } = require('http-status-codes');
+const { UserService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+/*
+ Post: /signup
+ req-body {email:'a@ab.com',password:"1223"}
+*/ 
+async function signUp(req,res){
+    try {
+        const user = await UserService.createUser({
+            email: req.body.email,
+            password: req.body.password
+        });
+        SuccessResponse.data = user;
+        return res.status(StatusCodes.CREATED).json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res.status(error.statusCode).json(ErrorResponse);
+    }
+}
+
+module.exports = {
+    signUp
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const { InfoController } = require('../../controllers');
+const UserRouter = require('./user-routes')
 
 const router = express.Router();
+
+router.use('/user',UserRouter);
 
 router.get('/info', InfoController.info);
 
