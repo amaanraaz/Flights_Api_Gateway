23a456857bb4e07050df16e852ae1e8cd4b73e7b
amaanraaj <amandwivedi00@gmail.com>
2024-01-23 00:48:02 +0530

    added basic proxy for flight and booking service using http-proxy-middleware package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const {rateLimit}  = require('express-rate-limit')
+const {rateLimit}  = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -14,6 +15,10 @@ const limiter = rateLimit({
 	limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes).
 });
 app.use(limiter);
+
+app.use('/flightService', createProxyMiddleware({ target: 'http://localhost:3000/', changeOrigin: true }));
+app.use('/bookingService', createProxyMiddleware({ target: 'http://localhost:4000/', changeOrigin: true }));
+
 
 app.use('/api', apiRoutes);
 app.listen(ServerConfig.PORT, () => {
